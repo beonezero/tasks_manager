950aa8ba2d632c1a84bbb26f614f6403031884b0
Neakhai Yauheni <beonezero@protonmail.com>
2025-05-25 22:34:51 +0300

    refactoring tasks-reducer.test

--- src/features/todolists/model/__tests__/tasks-reducer.test.ts
+++ src/features/todolists/model/__tests__/tasks-reducer.test.ts
@@ -1,10 +1,11 @@
 import { beforeEach, expect, test } from "vitest"
 
 import { v1 } from "uuid"
-import { addTask, changeTaskStatus, changeTaskTitle, removeTask, tasksReducer, TasksType } from "../tasks-slice.ts"
-import { addTodolist, removeTodolist } from "../todolists-slice.ts"
+import { addTask, removeTask, setTasks, tasksReducer, updateTask } from "../tasks-slice.ts"
+import { Tasks } from "@/features/todolists/api/tasksApi.types.ts"
+import { TaskPriority, TaskStatus } from "@/features/todolists/libs/enums.ts"
 
-let startState: TasksType
+let startState: Tasks
 const todolistId1 = v1()
 const todolistId2 = v1()
 
@@ -17,72 +18,78 @@ const taskId_5 = v1()
 beforeEach(() => {
   startState = {
     [todolistId1]: [
-      { id: taskId_1, title: "HTML&CSS", isDone: true },
-      { id: taskId_2, title: "JS", isDone: true },
-      { id: taskId_3, title: "ReactJS", isDone: false },
+      {description: null, title: "HTML&CSS", status: TaskStatus.New, priority: TaskPriority.Low, startDate: null, deadline: null, id: taskId_1, todoListId: todolistId1, order: 1,
+        addedDate: "10.10.2020"},
+      {description: null, title: "JS", status: TaskStatus.New, priority: TaskPriority.Low, startDate: null, deadline: null, id: taskId_2, todoListId: todolistId1, order: 1,
+        addedDate: "10.10.2020"},
+      {description: null, title: "React", status: TaskStatus.Completed, priority: TaskPriority.Low, startDate: null, deadline: null, id: taskId_3, todoListId: todolistId1, order: 1,
+        addedDate: "10.10.2020"},
+
     ],
     [todolistId2]: [
-      { id: taskId_4, title: "Rest API", isDone: true },
-      { id: taskId_5, title: "GraphQL", isDone: false },
+      {description: null, title: "Redux", status: TaskStatus.New, priority: TaskPriority.Low, startDate: null, deadline: null, id: taskId_4, todoListId: todolistId1, order: 1,
+        addedDate: "10.10.2020"},
+      {description: null, title: "Next", status: TaskStatus.Completed, priority: TaskPriority.Low, startDate: null, deadline: null, id: taskId_5, todoListId: todolistId1, order: 1,
+        addedDate: "10.10.2020"},
     ],
   }
 })
 
 test("whether it is correct to remove a task from todolist", () => {
-  const endState: TasksType = tasksReducer(startState, removeTask({ todolistId: todolistId1, taskId: taskId_2 }))
+  const endState: Tasks = tasksReducer(startState, removeTask({ todolistId: todolistId1, taskId: taskId_2 }))
 
   expect(endState[todolistId1].length).toBe(2)
   expect(endState[todolistId1][1].id).toBe(taskId_3)
 })
 
 test("correct task should be added to correct array", () => {
-  const newTitle = "newTitleTest"
-  const endState: TasksType = tasksReducer(startState, addTask({ todolistId: todolistId2, title: newTitle }))
+  const newTitle = "taskTitleTest"
+  const endState: Tasks = tasksReducer(startState, addTask({task: {description: null, title: newTitle, status: TaskStatus.New, priority: TaskPriority.Low, startDate: null, deadline: null, id: "taskIdTest", todoListId: todolistId2, order: 1,
+      addedDate: "10.10.2020"}}))
 
   expect(endState[todolistId1].length).toBe(3)
   expect(endState[todolistId2].length).toBe(3)
   expect(endState[todolistId2][0].title).toBe(newTitle)
-  expect(endState[todolistId2][0].id).toBeDefined()
-  expect(endState[todolistId2][0].isDone).toBeFalsy()
+  expect(endState[todolistId2][0].id).toBe("taskIdTest")
+  expect(endState[todolistId2][0].status).toBe(TaskStatus.New)
   // toBeFalsy() ===> toBe(false) ; toBeTruthy() ===> toBe(true)
   // .not.toBe("") принимаем всё, кроме пустой строки
 })
 
-test("status of specified task should be changed", () => {
-  const endState: TasksType = tasksReducer(
+test("update task", () => {
+  const endState: Tasks = tasksReducer(
     startState,
-    changeTaskStatus({ todolistId: todolistId1, taskId: taskId_2, isDone: false }),
+    updateTask({ task: {
+      description: null, title: "New JS", status: TaskStatus.Completed, priority: TaskPriority.Low, startDate: null, deadline: null, id: taskId_2, todoListId: todolistId1, order: 1,
+        addedDate: "10.10.2020"
+    }
+    }),
   )
 
   expect(endState[todolistId1].length).toBe(3)
-  expect(endState[todolistId1][1].isDone).toBeFalsy()
+  expect(endState[todolistId1][1].status).toBe(TaskStatus.Completed)
+  expect(endState[todolistId1][1].title).toBe("New JS")
 })
 
-test("title of specified task should be changed", () => {
-  const newTitle = "newTaskTitle"
-  const endState: TasksType = tasksReducer(
-    startState,
-    changeTaskTitle({ todolistId: todolistId2, taskId: taskId_4, title: newTitle }),
+test("set task", () => {
+  const endState: Tasks = tasksReducer(
+    {},
+    setTasks({ tasks: [
+      {
+        description: null, title: "RTK", status: TaskStatus.New, priority: TaskPriority.Low, startDate: null, deadline: null, id: "testTaskId1", todoListId: todolistId1, order: 1,
+        addedDate: "10.10.2020"
+      },
+        {
+          description: null, title: "GraphQL", status: TaskStatus.New, priority: TaskPriority.Low, startDate: null, deadline: null, id: "testTaskId2", todoListId: todolistId1, order: 1,
+          addedDate: "10.10.2020"
+        }
+      ],
+      todolistId: todolistId1
+    }),
   )
 
-  expect(endState[todolistId1].length).toBe(3)
-  expect(endState[todolistId2][0].title).toBe(newTitle)
-})
-
-test("new array should be added when new todolist is added", () => {
-  const endState = tasksReducer(startState, addTodolist("new todolist"))
-
-  const keys = Object.keys(endState)
-  const newKey = keys.find((k) => k !== todolistId1 && k !== todolistId2)
-  if (!newKey) {
-    throw Error("new key should be added")
-  }
-
-  expect(keys.length).toBe(3)
-  expect(endState[newKey]).toEqual([])
-})
-
-test("array should be deleted when a todolist is deleted", () => {
-  const endState = tasksReducer(startState, removeTodolist(todolistId1))
-  expect(endState[todolistId1]).toBeUndefined()
+  expect(endState[todolistId1].length).toBe(2)
+  expect(endState[todolistId1][0].id).toBe("testTaskId1")
+  expect(endState[todolistId1][1].title).toBe("GraphQL")
+  expect(endState[todolistId1][1].todoListId).toBe(todolistId1)
 })
