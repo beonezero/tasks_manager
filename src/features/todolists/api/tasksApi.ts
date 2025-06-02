0538397a14c16e5f0ff0c47e4827a0806861d172
Neakhai Yauheni <beonezero@protonmail.com>
2025-06-02 23:39:28 +0300

    delete taskApi axios

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,4 +1,3 @@
-import { instance } from "@/common/instance/instance.ts"
 import { GetTasksResponse, Task, UpdateTaskModel } from "@/features/todolists/api/tasksApi.types.ts"
 import { BaseResponse } from "@/common/types"
 import { baseApi } from "@/app/baseApi.ts"
@@ -48,24 +47,3 @@ export const taskApi = baseApi.injectEndpoints({
 
 export const { useGetTasksQuery, useCreateTaskMutation, useRemoveTaskMutation, useUpdateTaskMutation } = taskApi
 
-export const _tasksApi = {
-  getTasks: (todolistId: string) => {
-    return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
-  },
-  createTask: (payload: { todolistId: string; title: string }) => {
-    const { todolistId, title } = payload
-    return instance.post<BaseResponse<{ item: Task }>>(`todo-lists/${todolistId}/tasks`, { title })
-  },
-  removeTask: (payload: { todolistId: string; taskId: string }) => {
-    const { todolistId, taskId } = payload
-    return instance.delete<BaseResponse<{ item: Task }>>(`todo-lists/${todolistId}/tasks/${taskId}`)
-  },
-  updateTask: (payload: { todolistId: string; taskId: string; updateTaskModel: UpdateTaskModel }) => {
-    const { todolistId, taskId, updateTaskModel } = payload
-    return instance.put<
-      BaseResponse<{
-        item: Task
-      }>
-    >(`todo-lists/${todolistId}/tasks/${taskId}`, updateTaskModel)
-  },
-}
